92f9af5d6a977bb9f223993f19e465b0e23d05f6
ankitankit39457-spec <ankitankit39457@gmail.com>
2025-09-07 11:31:20 +0000

    02_datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all js code as newer version
+
+//alert(3+3)//n we are using node.js, not browser
+console.log(3+3)
+         //code readability should be high
+console.log("Ankit")
+
+
+let name =("Ankit")
+let age = 21
+let islogged = false
+
+//number => 2 to power 53
+//bigint
+//string => ""
+
+//boolean => true/false
+//null => standalone value
+//undefined =>
+    //symbol => unique
+
+//object
+
+
+console.log(typeof "hitesh")
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object

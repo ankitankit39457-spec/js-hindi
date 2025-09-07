0c7415ad45fa4816550f37f01fae431293457fc2
ankitankit39457-spec <ankitankit39457@gmail.com>
2025-09-07 07:54:27 +0000

    basics

---
+++ 01_basics/test.js
@@ -0,0 +1,15 @@
+const accountId = 12345
+let accountemail ="ankit@gmail.com"
+var accountpassword = "1234"
+accountcity = "Safidon"
+let accountstate;
+
+//accountId =2// not allowed in const
+
+accountEmail = "ak@gmail.com"
+accountpassword ="4312"
+accountcity="Jind"
+
+console.log(accountId)
+
+console.table([accountEmail, accountpassword, accountcity, accountstate])
